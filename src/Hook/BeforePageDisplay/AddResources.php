3da5219965e0b934c15d9a8fdb882ec80b2fb625
rvogel <vogel@hallowelt.biz>
2018-12-06 09:21:48 +0100

    Fix duplicate class definition
    NEEDS CHERRY-PICK TO REL1_31
    
    Change-Id: I57720e5cb590175c44985f03ccab365016525ac2

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -6,9 +6,13 @@ class AddResources extends \BlueSpice\Hook\BeforePageDisplay {
 
 	protected function doProcess() {
 		$this->out->addModuleStyles( 'ext.bluespice.statistics.styles' );
-		$title = $this->out->getTitle();
-		if( $title->isSpecial( 'AdminDashboard' ) || $title->isSpecial( 'UserDashboard' ) ) {
+		// Todo: collecting resources should be done client side by dashboards
+		foreach( ['AdminDashboard', 'UserDashboard', 'WikiAdmin'] as $special ) {
+			if( !$this->skin->getTitle()->isSpecial( $special ) ) {
+				continue;
+			}
 			$this->out->addModules( 'ext.bluespice.statisticsPortlets' );
+			break;
 		}
 		return true;
 	}
