e306c7517da16de1aea73c0b4d584fe8f2a1a4a8
Dejan Savuljesku <savuljesku@hallowelt.com>
2021-01-19 08:13:27 +0100

    Remove use of deprecated $wgHooks global for search
    [4.0]
    
    ERM 22587
    
    Change-Id: Icf024367df2145b51537318c9e3bc174d7f7e877

--- src/ExtendedSearch/Updater/Snapshot.php
+++ src/ExtendedSearch/Updater/Snapshot.php
@@ -6,11 +6,4 @@ use BS\ExtendedSearch\Source\Updater\Base as Updater;
 
 class Snapshot extends Updater {
 	protected $sJobClass = "\\BlueSpice\\ExtendedStatistics\\ExtendedSearch\\Job\\Snapshot";
-
-	/**
-	 *
-	 * @param array &$aHooks
-	 */
-	public function init( &$aHooks ) {
-	}
 }
