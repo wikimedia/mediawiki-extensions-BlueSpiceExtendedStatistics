e5f5c8cd043443add0a2be8328a63cf6359b2204
pavloyashchenko <pyashchenko@hallowelt.com>
2022-08-04 14:57:22 +0200

    Create AbstractSchema for sql table
    Create Json file; Generate 3 files(sqlite, postgres, mysql); Change path in addExtensionTable() to generated file.
    
    ERM29227
    
    [master]
    
    Change-Id: I46862423d3a3701d60331fbb1b7f679da315ff9c

--- src/Hook/LoadExtensionSchemaUpdates/AddSnapshotTable.php
+++ src/Hook/LoadExtensionSchemaUpdates/AddSnapshotTable.php
@@ -8,36 +8,40 @@ use DatabaseUpdater;
 class AddSnapshotTable extends LoadExtensionSchemaUpdates {
 
 	protected function doProcess() {
+		$dbType = $this->updater->getDB()->getType();
 		$dir = $this->getExtensionPath();
 
 		$this->updater->addExtensionTable(
 			'bs_extendedstatistics_snapshot',
-			"$dir/maintenance/db/bs_extendedstatistics_snapshot.sql"
+			"$dir/maintenance/db/sql/$dbType/bs_extendedstatistics_snapshot-generated.sql"
 		);
 
-		$this->updater->addExtensionField(
-			'bs_extendedstatistics_snapshot', 'ess_type',
-			"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.add_type.sql"
-		);
+		if ( $dbType == 'mysql' ) {
+			$this->updater->addExtensionField(
+				'bs_extendedstatistics_snapshot', 'ess_type',
+				"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.add_type.sql"
+			);
 
-		$this->updater->addExtensionField(
-			'bs_extendedstatistics_snapshot', 'ess_interval',
-			"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.add_interval.sql"
-		);
+			$this->updater->addExtensionField(
+				'bs_extendedstatistics_snapshot', 'ess_interval',
+				"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.add_interval.sql"
+			);
 
-		$this->updater->addExtensionField(
-			'bs_extendedstatistics_snapshot', 'ess_secondary_data',
-			"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.add_secondary_data.sql"
-		);
+			$this->updater->addExtensionField(
+				'bs_extendedstatistics_snapshot', 'ess_secondary_data',
+				"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.add_secondary_data.sql"
+			);
 
-		$this->updater->dropExtensionField(
-			'bs_extendedstatistics_snapshot', 'ess_id',
-			"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.remove_id.sql"
-		);
-		$this->updater->modifyExtensionField(
-			'bs_extendedstatistics_snapshot', 'ess_data',
-			"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.modify_data.sql"
-		);
+			$this->updater->dropExtensionField(
+				'bs_extendedstatistics_snapshot', 'ess_id',
+				"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.remove_id.sql"
+			);
+			$this->updater->modifyExtensionField(
+				'bs_extendedstatistics_snapshot', 'ess_data',
+				"$dir/maintenance/db/bs_extendedstatistics_snapshot.patch.modify_data.sql"
+			);
+		}
+
 		$this->updater->addExtensionUpdate( [ [ $this, 'removeOldData' ] ] );
 	}
 
