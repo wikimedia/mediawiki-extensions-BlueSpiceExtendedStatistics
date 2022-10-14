e766b57377ed33c83ee371c0b40a29726f832c38
miriam schlindwein <schlindwein@hallowelt.com>
2022-10-14 11:31:04 +0200

    Add export button plugin
    ERM29537
    
    [4.2.x]
    
    Change-Id: I3590b3f4516e76fe90e009cff796ac9df02c9c17
    (cherry picked from commit fd1648ade7936c7f55d8ef8017a6e447176ea016)
    (cherry picked from commit ca83f94e2faf984b66392d9d5d5256b28fd97242)

--- resources/bluespice.aggregatedstatistics.js
+++ resources/bluespice.aggregatedstatistics.js
@@ -1,5 +1,6 @@
 $( function() {
 	var $mainContainer = $( '#bs-extendedstatistics-special-aggregatedstatistics' );
+	var pluginModules = require( './pluginModules.json' );
 
 	var reportHandlers =  $mainContainer.attr( 'data-reports' ),
 		registry = new bs.aggregatedStatistics.ReportRegistry( JSON.parse( reportHandlers ) ),
@@ -26,6 +27,7 @@ $( function() {
 		disabled: true
 	} ),
 		panel = new OO.ui.PanelLayout( { expanded: false });
+
 	var mainLayout = new OO.ui.FieldsetLayout( {
 		label: mw.message( "bs-statistics-aggregated-report-picker-label" ).text(),
 		align: 'top',
@@ -39,16 +41,23 @@ $( function() {
 
 	dropdown.getMenu().connect( this, {
 		select: function( selected ) {
-		   if ( selected instanceof OO.ui.MenuOptionWidget ) {
+			if ( selected instanceof OO.ui.MenuOptionWidget ) {
 				var cls = registry.lookup( selected.getData() );
-			   var item = new cls( {
-				   defaultFilterValues: defaultFilters,
-				   name: selected.getData()
-			   } );
-			   panel.$element.html( item.$element );
-		   }
-		}
-   } );
+				var item = new cls( {
+					defaultFilterValues: defaultFilters,
+					name: selected.getData()
+				} );
 
+				panel.$element.html( item.$element );
+				item.connect( this, {
+					dataset: function ( data ) {
+						mw.loader.using( pluginModules ).done( function () {
+							mw.hook( 'aggregatedstatistics.addUI' ).fire( data );
+						} );
+					}
+				} );
+			}
+		}
+	} );
 
 })
