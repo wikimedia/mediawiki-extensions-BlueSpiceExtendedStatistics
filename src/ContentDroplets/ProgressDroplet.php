93b15bf05fe4c9488161715cfed7221b8e686c38
hslater <hamish.slater@hallowelt.com>
2025-01-22 10:30:55 +0100

    Update Message namespace
    ERM38444
    
    [REL1_43, master]

--- src/ContentDroplets/ProgressDroplet.php
+++ src/ContentDroplets/ProgressDroplet.php
@@ -3,7 +3,7 @@
 namespace BlueSpice\ExtendedStatistics\ContentDroplets;
 
 use MediaWiki\Extension\ContentDroplets\Droplet\TagDroplet;
-use Message;
+use MediaWiki\Message\Message;
 
 class ProgressDroplet extends TagDroplet {
 
