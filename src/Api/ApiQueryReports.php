4c1ee580195259d72e472dc529a08e6e20e2cfdb
hslater <hamish.slater@hallowelt.com>
2025-01-29 09:47:06 +0100

    Update ApiQueryBase namespace
    ERM38444
    
    [REL1_43, master]

--- src/Api/ApiQueryReports.php
+++ src/Api/ApiQueryReports.php
@@ -12,10 +12,11 @@ use DateInterval;
 use InvalidArgumentException;
 use MediaWiki\Api\ApiBase;
 use MediaWiki\Api\ApiQuery;
+use MediaWiki\Api\ApiQueryBase;
 use MediaWiki\MediaWikiServices;
 use Wikimedia\ParamValidator\ParamValidator;
 
-class ApiQueryReports extends \ApiQueryBase {
+class ApiQueryReports extends ApiQueryBase {
 	/** @var AttributeRegistryFactory */
 	private $providerFactory;
 	/** @var ISnapshotStore */
